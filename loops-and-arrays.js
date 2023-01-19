147645914ac79a085f33cc08f2aaadc749ced7fa
lexdolfing <lexdolfing@gmail.com>
2023-01-19 17:16:34 +0100

    Main leeg gemaakt

--- loops-and-arrays.js
+++ loops-and-arrays.js
@@ -4,14 +4,6 @@
 // Zorg ervoor dat dit ook zou werken als de array wel 100 of 200 namen zou bevatten!
 // ==========================================
 
-let names = ["Henk", "Piet", "Fred", "Joop"];
-
-
-for (let i = 0; i < names.length; i++){
-    names[i] = names[i]+"je";
-    console.log(names[i]);
-}
-
 
 // Verwachtte uitkomsten:
 // Vóór het script zie je de originele waardes:
@@ -28,20 +20,6 @@ for (let i = 0; i < names.length; i++){
 // Zorg ervoor dat dit ook zou werken als de array wel 100 of 200 getallen zou bevatten!
 // ==========================================
 
-const numbers = [2, 4, 5, 29, 38];
-console.log(numbers.length);
-let doubleNumbers = [];
-
-for (let i = 0; i < numbers.length; i++){
-    if (numbers[i]%2===0){
-        doubleNumbers[i] = numbers[i]*2;
-    }
-    else {
-        doubleNumbers[i] = numbers[i]*3;
-    }
-}
-
-console.log(doubleNumbers);
 
 
 // Verwachtte uitkomsten:
@@ -61,13 +39,7 @@ console.log(doubleNumbers);
 // Zorg ervoor dat dit ook zou werken als de array wel 100 of 200 getallen zou bevatten!
 // ==========================================
 
-const squares = [30, 2, 8, 24, 11];
-let volumesOfCubes =[];
 
-for (let i = 0; i < squares.length; i++){
-    volumesOfCubes[i] = "Het volume van " + squares[i] + " is " + squares[i]*squares[i]*squares[i];
-}
-console.log(volumesOfCubes)
 
 // Verwachtte uitkomsten:
 // Vóór het script zie je de originele waardes:
--- loops.js
+++ loops.js
@@ -6,10 +6,6 @@
 // Hoera!
 // ==========================================
 
-for (let i = 0; i <3; i++){
-    console.log("Hoera!")
-}
-
 
 // ==========================================
 // Opdracht 2. Maak een for-loop die 4 keer het woord "loop..." logt, en bij de vijfde loop het woord "klaar!"
@@ -20,14 +16,6 @@ for (let i = 0; i <3; i++){
 // loop...
 // klaar!
  // ==========================================
-
-for (let i = 0; i <5; i++) {
-if (i <4){
-
-    console.log("loop...");}
-else {
-    console.log("klaar!")}
-}
 
 
 // ==========================================
@@ -43,14 +31,6 @@ else {
 // Tip: een getal is oneven als je het door 2 deelt en er blijft nog iets over. Een getal is even als je het deelt door 2 en er blijft niets over. Google daarom eens naar 'mdn webdocs remainder operator'.
 // ==========================================
 
-for (let i = 0; i<16; i++){
-    if (i%2 === 0){
-        console.log(i + " is even");
-    }
-    if (i%2 !== 0){
-        console.log(i + " is oneven")
-    }
-}
 
 
 // ==========================================
@@ -68,15 +48,3 @@ for (let i = 0; i<16; i++){
 // >> 8
 // >> 9
 // ==========================================
-
-for (let i = 0; i<10; i++){
-    if (i<3){
-    console.log(i);}
-    else if (i>=3 && i < 6){
-        console.log(">" + i)
-    }
-    else{
-        console.log(">>" + i);
-    }
-
-}
